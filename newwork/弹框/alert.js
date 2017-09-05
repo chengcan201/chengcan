2c9dd7d6ef9770bcf0821b145638e29e1dcf69ce
chengcan <can.cheng@haituncun.com>
2017-09-05 11:09:33 +0800

    提交新文文件

---
+++ newwork/弹框/alert.js
@@ -0,0 +1,119 @@
+var popup={
+	customAlert:function(msg){
+		this.addAlert(msg);
+		
+		var alertCloseBtn=document.getElementById("alert-close-btn"),
+			alertConfirmBtn=document.getElementById("alert-confirm-btn");
+			
+		this.addEvent(alertCloseBtn,"click",this.closePopUp);
+		this.addEvent(alertConfirmBtn,"click",this.closePopUp);
+		
+	},
+	customConfirm:function(msg,okCallBack){
+		this.addConfirm(msg);//插入html中，下面才可获取到ID，位置不可与下面掉换；
+		
+		var alertCloseBtn = document.getElementById("alert-close-btn"),
+            alertConfirmBtn = document.getElementById("alert-confirm-btn"),
+            alertCancelBtn = document.getElementById("alert-cancel-btn");
+		this.addEvent(alertCloseBtn,"click",this.closePopUp);
+		this.addEvent(alertCancelBtn, "click", this.closePopUp);
+        this.addEvent(alertConfirmBtn, "click", function(){
+            popup.closePopUp();
+            okCallBack();
+        });
+		
+	},
+	addAlert:function(msg){//添加页面结构
+		var layer=document.createElement("div");
+		layer.setAttribute("class","alert-layer");
+		layer.setAttribute("id","alert-layer");
+		
+		var alert=document.createElement("div");
+		alert.setAttribute("class","alert-blk");
+		alert.setAttribute("id","alert-blk");
+		
+		var html = "";
+        html += '<div class="alert-title-fixed">';
+        html += '<span class="alert-title">警告</span>';
+        html += '<span class="alert-close-btn" id="alert-close-btn"></span>';
+        html += '</div>';
+        html += '<div class="alert-msg-fixed">';
+        html += '<span class="alert-icon alert-icon-warn"></span>';
+        html += '<span class="alert-msg">' + msg + '</span>';
+        html += '</div>';
+        html += '<div class="alert-bottom">';
+        html += '<span class="alert-confirm-btn" id="alert-confirm-btn">确定</span>';
+        html += '</div>';
+        alert.innerHTML = html;
+
+        document.body.appendChild(layer);
+        document.body.appendChild(alert);
+	},
+	addEvent:function(ele,evtName,callback){
+		if(document.addEventListener){
+			ele.addEventListener(evtName,callback);
+		}else{
+			ele.attachEvent("on"+evtName,callback);
+		}
+	},
+	
+    customMsg:function(msg){//添加页面结构
+		var layer=document.createElement("div");
+		layer.setAttribute("class","alert-layer");
+		layer.setAttribute("id","alert-layer");
+		
+		var alert=document.createElement("div");
+		alert.setAttribute("class","msg-blk");
+		alert.setAttribute("id","msg-blk");
+		
+		var html = "";
+		html += '<div class="alert-msg-fixed">';
+        html += '<span class="alert-icon alert-icon-warn"></span>';
+        html += '<span class="alert-msg">' + msg + '</span>';
+        html += '</div>';
+        alert.innerHTML = html;
+        document.body.appendChild(layer);
+        document.body.appendChild(alert);
+        
+        setTimeout(function(){
+	        	document.body.removeChild(layer);
+	        document.body.removeChild(alert);
+        },1000)
+	},
+	addConfirm:function(msg){
+        var layer = document.createElement("div");
+        layer.setAttribute("class", "alert-layer");
+        layer.setAttribute("id", "alert-layer");
+
+        var alert = document.createElement("div");
+        alert.setAttribute("class", "alert-blk");
+        alert.setAttribute("id", "alert-blk");
+
+        var html = "";
+
+        html += '<div class="alert-title-fixed">';
+        html += '<span class="alert-title">系统提示</span>';
+        html += '<span class="alert-close-btn" id="alert-close-btn"></span>';
+        html += '</div>';
+        html += '<div class="alert-msg-fixed">';
+        html += '<span class="alert-icon alert-icon-ask"></span>';
+        html += '<span class="alert-msg">' + msg + '</span>';
+        html += '</div>';
+        html += '<div class="alert-bottom">';
+        html += '<span class="alert-confirm-btn" id="alert-confirm-btn">确定</span>';
+        html += '<span class="alert-cancel-btn" id="alert-cancel-btn">取消</span>';
+        html += '</div>';
+
+        alert.innerHTML = html;
+
+        document.body.appendChild(layer);
+        document.body.appendChild(alert);
+    },
+    closePopUp:function(){
+        var layer=document.getElementById("alert-layer"),
+            alert=document.getElementById("alert-blk");
+
+        document.body.removeChild(layer);
+        document.body.removeChild(alert);
+    },
+}
